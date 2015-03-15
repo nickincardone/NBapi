2d4c26e46750d268f79cdb3bc5542619a4ffc3e6
Nick Incardone <nickincardone@gmail.com>
2015-03-14 20:28:38 -0400

    added more endpoints

--- nba/endpoints.py
+++ nba/endpoints.py
@@ -3,6 +3,13 @@ nba_api = 'http://stats.nba.com/stats/'
 schedule_endpoint = nba_api+'scoreboardv2'
 boxscore_endpoint = nba_api+'boxscoretraditionalv2'
 team_schedule_endpoint = nba_api+'teamgamelog'
+player_info = nba_api+'commonplayerinfo'
+player_game_log = nba_api+'playergamelog'
+team_info = nba_api+'teaminfocommon'
+all_players = nba_api+'commonallplayers'
+team_roster = nba_api+'commonteamroster'
+player_shot_log = nba_api+'playerdashptshotlog'
+player_rebound_log = nba_api='playerdashptreboundlogs'
 
 #known enpoints
 '''
@@ -19,6 +26,7 @@ http://stats.nba.com/stats/boxscoreusage
 http://stats.nba.com/stats/boxscoremisc
 http://stats.nba.com/stats/boxscoreadvanced
 http://stats.nba.com/stats/boxscorefourfactors
+http://stats.nba.com/feeds/RotoWirePlayers-583598/team_id.json
 '''
 
 def get_schedule(date):
@@ -52,4 +60,89 @@ def get_boxscore(game_id):
         'RangeType': 2
     }
     r = requests.get(boxscore_endpoint, params=payload)
+    return r.json()
+
+def get_player_info(player_id):
+    # example james harden = 201935
+    payload = {
+        "PlayerID":player_id,
+        "LeagueID":"00"
+    }
+    r = requests.get(player_info, params=payload)
+    return r.json()
+
+def get_player_games(player_id, season):
+    payload = {
+        "PlayerID":player_id,
+        "LeagueID":"00",
+        "Season":season,
+        "SeasonType":"Regular Season"
+    }
+    r = requests.get(player_game_log, params=payload)
+    return r.json()
+
+def get_team_info(team_id, season):
+    payload = {"LeagueID":"00","Season":season,"SeasonType":"Regular Season","TeamID":team_id}
+    r = requests.get(team_info, params=payload)
+    return r.json()
+
+def get_team_roster(team_id, season):
+    payload = {"TeamID":team_id,"LeagueID":"00","Season":season}
+    r = requests.get(team_roster, params=payload)
+    return r.json()
+
+def get_player_shot_log(player_id):
+    payload = {
+        "LeagueID":"00",
+        "Season":"2014-15",
+        "SeasonType":"Regular Season",
+        "PlayerID":player_id,
+        "TeamID":0,
+        "Outcome":'null',
+        "Location":'null',
+        "Month":0,
+        "SeasonSegment":'null',
+        "DateFrom":'null',
+        "DateTo":'null',
+        "OpponentTeamID":0,
+        "VsConference":'null',
+        "VsDivision":'null',
+        "GameSegment":'null',
+        "Period":0,
+        "LastNGames":0
+    }
+    r = requests.get(player_shot_log, params=payload)
+    return r.json()
+
+def get_player_rebound_log(player_id):
+    payload = {
+        "LeagueID":"00",
+        "Season":"2014-15",
+        "SeasonType":"Regular Season",
+        "PlayerID":player_id,
+        "TeamID":0,
+        "Outcome":'null',
+        "Location":'null',
+        "Month":0,
+        "SeasonSegment":'null',
+        "DateFrom":'null',
+        "DateTo":'null',
+        "OpponentTeamID":0,
+        "VsConference":'null',
+        "VsDivision":'null',
+        "GameSegment":'null',
+        "Period":0,
+        "LastNGames":0
+    }
+    r = requests.get(player_rebound_log, params=payload)
+    return r.json()
+
+def get_all_players(season, all_time=False):
+    # 0 is false 1 is true
+    cur_season = 1
+    if all_time:
+        cur_season = 0
+
+    payload = {"LeagueID":"00","Season":season,"IsOnlyCurrentSeason":cur_season}
+    r = requests.get(all_players, params=payload)
     return r.json()
