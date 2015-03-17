1ca2dff263f46291f282983a270fe82268cbb2d9
Nick Incardone <nickincardone@gmail.com>
2015-03-17 00:05:59 -0400

    added dict of team_ids and helper function to get and cleanse team inputs

--- nba/utils.py
+++ nba/utils.py
@@ -1,6 +1,100 @@
 import datetime
 import requests
 
+_team_ids = {
+    'atlanta': 1610612737,
+    'cleveland': 1610612739,
+    'toronto': 1610612761,
+    'chicago': 1610612741,
+    'washington': 1610612764,
+    'milwaukee': 1610612749,
+    'indiana': 1610612754,
+    'charlotte': 1610612766,
+    'boston': 1610612738,
+    'miami': 1610612748,
+    'brooklyn': 1610612751,
+    'detroit': 1610612765,
+    'orlando': 1610612753,
+    'philadelphia': 1610612755,
+    'new york': 1610612752,
+    'golden state': 1610612744,
+    'memphis': 1610612763,
+    'portland': 1610612757,
+    'houston': 1610612745,
+    'san antonio': 1610612759,
+    'dallas': 1610612742,
+    'l.a. clippers': 1610612746,
+    'oklahoma city': 1610612760,
+    'new orleans': 1610612740,
+    'phoenix': 1610612756,
+    'utah': 1610612762,
+    'denver': 1610612743,
+    'sacramento': 1610612758,
+    'l.a. lakers': 1610612747,
+    'minnesota': 1610612750
+}
+
+_other_team_names = {
+    'atl': 'atlanta',
+    'cle': 'cleveland',
+    'tor': 'toronto',
+    'chi': 'chicago',
+    'was': 'washington',
+    'mil': 'milwaukee',
+    'ind': 'indiana',
+    'cha': 'charlotte',
+    'bos': 'boston',
+    'mia': 'miami',
+    'brk': 'brooklyn',
+    'det': 'detroit',
+    'orl': 'orlando',
+    'phi': 'philadelphia',
+    'nyk': 'new york',
+    'ny': 'new york',
+    'gs': 'golden state',
+    'gsw': 'golden state',
+    'mem': 'memphis',
+    'por': 'portland',
+    'hou': 'houston',
+    'sa': 'san antonio',
+    'sas': 'san antonio',
+    'dal': 'dallas',
+    'lac': 'l.a. clippers',
+    'la clippers': 'l.a. clippers',
+    'okc': 'oklahoma city',
+    'no': 'new orleans',
+    'nop': 'new orleans',
+    'pho': 'phoenix',
+    'uta': 'utah',
+    'den': 'denver',
+    'sac': 'sacramento',
+    'lal': 'l.a. lakers',
+    'la lakers': 'l.a. lakers',
+    'min': 'minnesota'
+}
+
+def get_team_id(team):
+    team_clean = team.lower()
+    if team_clean in _other_team_names:
+        full_name = _other_team_names[team_clean]
+        return _team_ids[full_name]
+    if team_clean in _team_ids:
+        return _team_ids[team]
+    return None
+
+def cleanse_team(team):
+    if team in _team_ids.values():
+        return team
+    if type(team) is not str:
+        return None
+    if team.isdigit():
+        if int(team) in _team_ids.values():
+            return int(team)
+        else:
+            return None
+    if team_id = get_team_id(team):
+        return team_id
+
 def cleanse_date(date):
     #TODO
     pass
